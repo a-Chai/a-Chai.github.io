49447e7771f5f0f8838d6dd754963d09e5973ecf
Anthony Wong <wlvanthony@gmail.com>
2016-05-26 12:33:12 +0800

    css animation change to javascript jq

---
+++ js2/theatre_theme.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+
+	var d_background = 1500,
+		d_welcome = 1500,
+		d_myname = 1500,
+		d_button = 3000;
+
+	var que = [
+			{selector: ".section1.background", 	time: 1000, duration: d_background},
+			{selector: ".page.title span.t1", 	time: 500, duration: d_welcome},
+			{selector: ".page.title span.t2", 	time: 0, duration: d_myname},
+			{
+				selector1: ".aboutme .button2", 
+					time1: 0, duration1: d_button,
+				selector2: ".partner .button2, .ability .button2", 	
+					time2: 300, duration2: d_button,
+				selector3: ".hkt .button2, .exicon .button2, .greensward .button2, .scmp .button2",
+					time3: 600, duration3: d_button,
+				chain: 0
+			}
+		];
+
+	queing(que);
+
+	//$(".section1.background").delay(2000).fadeIn(1500);
+
+
+    $(".button").click(function(e){
+    });
+});
+
+
+function queing(q, it) {
+	if (it === undefined) it = 0;
+
+	if (q[it]["selector"])
+		$(q[it]["selector"])
+			.delay(q[it]["time"])
+			.fadeIn(q[it]["duration"],
+				q[ it+1 ] ? function(){ queing(q, it+1)}: undefined);
+	else if (q[it]["selector1"]) {
+		var par = 1;
+
+		while (q[it]["selector" + par]) {
+			console.log("I am Here~!");
+			$(q[it]["selector" + par])
+				.delay(q[it]["time" + par])
+				.fadeIn(q[it]["duration" + par],
+					(q[it]["chain"] === it && q[ it+1 ]) ? function(){ queing(q, it+1)}: undefined);
+			par++;
+		}
+	}
+}
